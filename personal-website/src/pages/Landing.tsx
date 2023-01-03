793c482689786ef619960b807f58b038bb231c46
„Irish-77“ <bastian.berle@gmail.com>
2023-01-03 20:32:01 +0100

    temp landing page

---
+++ personal-website/src/pages/Landing.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis vitae laudantium maiores mollitia ipsam blanditiis distinctio tempore aspernatur odit excepturi. Eveniet nostrum blanditiis amet molestias est ex asperiores vitae aspernatur!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, neque blanditiis! Inventore eum labore dolore tenetur, ab pariatur accusamus dignissimos quisquam delectus officiis perferendis provident et non doloribus alias ex.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia eos eveniet quibusdam! Officia corporis omnis id minima, laboriosam labore, quo voluptates sunt pariatur nulla earum illum nostrum provident molestiae debitis.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo hic commodi deserunt iusto nulla, delectus, aliquam veritatis nobis, veniam ut architecto modi culpa ratione at similique blanditiis illo ducimus nam!
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias explicabo quibusdam pariatur in odio nesciunt, illum sunt cum similique tempora maxime vitae ex magnam suscipit vero eveniet recusandae eum incidunt!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto error pariatur placeat adipisci aliquam harum suscipit hic dolores, deserunt, ad quae itaque. Corrupti assumenda sit qui repudiandae laudantium impedit laborum!
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, ratione aspernatur numquam cumque pariatur in aut maxime nihil aperiam eum totam ducimus aliquam! Tempora iusto, sapiente ducimus sed tenetur veritatis.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium voluptatem, ipsam voluptates suscipit veritatis temporibus exercitationem qui, laborum quis nesciunt modi ut officia itaque? Odio at asperiores sit praesentium iusto!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, fuga. Quidem adipisci velit quas iusto reprehenderit dolorum dicta odio cumque dignissimos hic aliquid, laudantium dolor qui blanditiis id nihil ratione!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eveniet rem voluptas dolor maxime, cupiditate neque iusto consequatur, illum accusantium, tempore atque doloribus aliquid minima repellat ad! Tempora, molestiae inventore!</h1>
+    </div>
+  )
+}
+
+export default Landing
