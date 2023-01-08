5a210ecf2e97f88df05c777dfb9f32f124638d26
„Irish-77“ <bastian.berle@gmail.com>
2023-01-08 22:39:52 +0100

    dummy code to test markdown rendering in prod

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import rehypeRaw from 'rehype-raw';
+
+
+import 'katex/dist/katex.min.css';
+
+import PacmanImage from '../assets/projects/project_image_pacman.jpeg';
 
 function Blog() {
+
+  const formula =  `
+  Given a **formula** below $$\\frac{1}{2}$$
+  
+  Calculate the value of $s$ when $u = 10\\frac{m}{s}$ and $a = 2\\frac{m}{s^{2}}$ at $$t = 1s$$
+
+  <img src="/project_image_pacman.jpeg" alt="Text">
+  `;
+
   return (
-    <div>Blog</div>
+    <div>
+      <ReactMarkdown
+        children={ formula }
+        remarkPlugins={[remarkMath]}
+        rehypePlugins={[rehypeKatex, rehypeRaw]}
+      />
+    </div>
   )
 }
 
