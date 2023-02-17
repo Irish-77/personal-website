a74c10847e7a5df2cb31417ae688255b8cf95365
„Irish-77“ <bastian.berle@gmail.com>
2023-02-17 23:05:45 +0100

    added landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,19 +1,43 @@
-import React from 'react'
+import React from 'react';
+
+import Typical from 'react-typical';
+import ParticlesBackground from '../components/ParticlesBackground';
+
+import my_image from './../assets/landing/me.jpg';
+
+import './../styles/Landing.css'
+
+
+
+const steps = [
+  'Student 👨🏻‍🎓', 2000,
+  'Programmer 💻', 2000,
+  'Data Scientist 🧮', 2000,
+  'ML/AI-Enthusiast 🧠', 2000,
+];
 
 function Landing() {
   return (
     <div>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis vitae laudantium maiores mollitia ipsam blanditiis distinctio tempore aspernatur odit excepturi. Eveniet nostrum blanditiis amet molestias est ex asperiores vitae aspernatur!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, neque blanditiis! Inventore eum labore dolore tenetur, ab pariatur accusamus dignissimos quisquam delectus officiis perferendis provident et non doloribus alias ex.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia eos eveniet quibusdam! Officia corporis omnis id minima, laboriosam labore, quo voluptates sunt pariatur nulla earum illum nostrum provident molestiae debitis.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo hic commodi deserunt iusto nulla, delectus, aliquam veritatis nobis, veniam ut architecto modi culpa ratione at similique blanditiis illo ducimus nam!
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias explicabo quibusdam pariatur in odio nesciunt, illum sunt cum similique tempora maxime vitae ex magnam suscipit vero eveniet recusandae eum incidunt!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto error pariatur placeat adipisci aliquam harum suscipit hic dolores, deserunt, ad quae itaque. Corrupti assumenda sit qui repudiandae laudantium impedit laborum!
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, ratione aspernatur numquam cumque pariatur in aut maxime nihil aperiam eum totam ducimus aliquam! Tempora iusto, sapiente ducimus sed tenetur veritatis.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium voluptatem, ipsam voluptates suscipit veritatis temporibus exercitationem qui, laborum quis nesciunt modi ut officia itaque? Odio at asperiores sit praesentium iusto!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, fuga. Quidem adipisci velit quas iusto reprehenderit dolorum dicta odio cumque dignissimos hic aliquid, laudantium dolor qui blanditiis id nihil ratione!
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi eveniet rem voluptas dolor maxime, cupiditate neque iusto consequatur, illum accusantium, tempore atque doloribus aliquid minima repellat ad! Tempora, molestiae inventore!</h1>
+
+      {/* Main */}
+      <div className="main-landing-container">
+        <div className="wrapper">
+          <div className="image-container">
+            <img src={my_image} alt="Portfolio" className="personal-image" />
+          </div>
+          <div className="text-container">
+            <h1 className="main-header">Hey, I'm Basti</h1>
+            <p>
+              I'm a {" "}
+              <Typical wrapper="b" steps={steps} loop={1} className="typical" />
+            </p>
+          </div>
+        </div>
+      </div>
+
     </div>
+
   )
 }
 
