ea4265c63002d659c047aa077aa6dce3ba0622ae
„Irish-77“ <bastian.berle@gmail.com>
2023-03-30 16:53:48 +0200

    update links for projects

--- src/services/ProjectList.tsx
+++ src/services/ProjectList.tsx
@@ -21,7 +21,7 @@ export const ProjectList = [
         image: ALKA,
         skills: 'Computer Vision, Control Theory, Autonomous Driving',
         stack: 'Python, Numpy, OpenCV, Carla, PID-Controller, Matplotlib',
-        repoURL: 'https://github.com/Ronho/active-lane-keeping-assistant',
+        repoURL: 'https://github.com/C2G-BR/Active-Lane-Keeping-Assistant',
         description: 'The ALKA project deals with the problem of computer-controlled tracking. In the project, PID, P, PD controllers were compared with each other. The track detection was done by different image transformations. The implementation was done with the simulation platform CARLA. Full documentation is available on Github.'        
     },
     {
@@ -39,16 +39,16 @@ export const ProjectList = [
         image: PacmanImage,
         skills: 'Python, Reinforcment Learning',
         stack: 'PyTorch, TensorBoard, Matplotlib, Numpy',
-        repoURL: 'https://github.com/Irish-77/pacman-ai3',
+        repoURL: 'https://github.com/C2G-BR/Pac-Man-AI',
         description: 'This project aim was to create a Pac-Man environment for reinforcement learning and solve this environment by applying Deep Q-Networks. Thereby the environment was developed from scratch and it allows custom maps.'
     },
     {
-        name: 'AIC Secruity',
+        name: 'Drone Secruity',
         context: 'University',
         image: AIC,
         skills: 'Arduino, Sensor, Tello Drone, IoT, Object Detection (YOLO)',
         stack: 'Python, Flask, Javascript/HTML, C++',
-        repoURL: 'https://github.com/Ronho/aic_security',
+        repoURL: 'https://github.com/C2G-BR/Drone-Security',
         description: 'AIC Security is a home security system that connects different sensors and systems. As the owner leaves his house, he activates the Guard mode by pressing a button. The motion sensor is then activated. When motion is detected, a drone is launched to take aerial photos. These are transmitted directly to the customer dashboard, where they are streamed live. Using object detection, people are directly marked on the live stream.'
         
     },
@@ -58,7 +58,7 @@ export const ProjectList = [
         image: C2G,
         skills: 'Web Development (Full-Stack), Chatbot, Recommder System, Social Networks - Page Rank, Collaborative Filtering, Chatbot',
         stack: 'Python, Flask, Angular 2, PostgresSQL, Numpy, Docker',
-        repoURL: 'https://github.com/Irish-77/C2G-Goes-AI',
+        repoURL: 'https://github.com/C2G-BR/Coin2Gether',
         description: "Coin2Gether (C2G) is a social trading platform where crypto enthusiasts can share information about the current trends in cryptocurrencies. It includes features such as a self-written chatbot that is able to carry out the user's commands on the website as well as a recommender system that suggests users to follow based on social networks and similar portfolios. Furthermore, API statistics can be viewed via an admin dashboard."
         
     },
@@ -78,7 +78,7 @@ export const ProjectList = [
         image: ICS,
         skills: 'Fullstack, Reinforcement Learning',
         stack: 'Python, Flask, PyTorch, Numpy, Angular 2, Ngrok',
-        repoURL: 'https://github.com/Ronho/intelligent-charging-station',
+        repoURL: 'https://github.com/C2G-BR/Intelligent-Charging-Station',
         description: 'ICS is a platform designed to provide intelligent charging to charging station operators in order to further minimize grid utilization.  In the process, a custom environment was developed on which an agent was trained using reinforcement learning.   In a comparison, the trained AI outperforms a first-come-first-served algorithm. In addition, the business is offered a front-end interface that provides a visual overview of the charging stations and the charging progression of the connected vehicles.'
     },
 ]
